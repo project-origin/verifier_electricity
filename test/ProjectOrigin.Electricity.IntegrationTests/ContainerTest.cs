f272881c6efe5bfcd491a77d73cbcdd9372ce5fa
Martin Schmidt <martin@scoding.dk>
2024-09-19 08:35:47 +0000

    Updated test

--- test/ProjectOrigin.Electricity.IntegrationTests/ContainerTest.cs
+++ test/ProjectOrigin.Electricity.IntegrationTests/ContainerTest.cs
@@ -7,17 +7,36 @@ using System.Text;
 using Grpc.Net.Client;
 using ProjectOrigin.TestCommon;
 using System.IO;
-using ProjectOrigin.Electricity.IntegrationTests.Fixtures;
+using DotNet.Testcontainers.Images;
+using Xunit.Abstractions;
+using Docker.DotNet;
 
 namespace ProjectOrigin.Electricity.IntegrationTests;
 
-public class ContainerTest : AbstractFlowTest, IAsyncLifetime, IClassFixture<ContainerImageFixture>
+public class ContainerTest : AbstractFlowTest, IAsyncLifetime
 {
+    private const string DockerfilePath = "Electricity.Dockerfile";
     private const int GrpcPort = 5000;
-    private readonly IContainer _container;
 
-    public ContainerTest(ContainerImageFixture imageFixture)
+    private readonly ITestOutputHelper _outputHelper;
+    private readonly IFutureDockerImage _image;
+    private readonly IContainer _container;
+    private readonly string _tempDockerPath;
+
+    public ContainerTest(ITestOutputHelper outputHelper)
     {
+        _outputHelper = outputHelper;
+        var solutionDirectory = CommonDirectoryPath.GetSolutionDirectory().DirectoryPath;
+
+        // Testcontainers doesn't support some functionality in Dockerfiles
+        _tempDockerPath = CreateTempDockerfileForTestcontainer(Path.Combine(solutionDirectory, DockerfilePath));
+
+        _image = new ImageFromDockerfileBuilder()
+            .WithDockerfileDirectory(solutionDirectory)
+            .WithDockerfile(Path.GetRelativePath(solutionDirectory, _tempDockerPath))
+            .WithLogger(new CustomActionLogger("ImageBuilder", outputHelper.WriteLine))
+            .Build();
+
         var configFile = TempFile.WriteAllText($"""
         registries:
           {Registry}:
@@ -29,7 +48,7 @@ public class ContainerTest : AbstractFlowTest, IAsyncLifetime, IClassFixture<Con
         """, ".yaml");
 
         _container = new ContainerBuilder()
-                .WithImage(imageFixture.Image)
+                .WithImage(_image)
                 .WithPortBinding(GrpcPort, true)
                 .WithResourceMapping(configFile, $"/app/tmp/")
                 .WithEnvironment("Network__ConfigurationUri", $"file:///app/tmp/{Path.GetFileName(configFile)}")
@@ -38,7 +57,43 @@ public class ContainerTest : AbstractFlowTest, IAsyncLifetime, IClassFixture<Con
                     Wait.ForUnixContainer()
                         .UntilPortIsAvailable(GrpcPort)
                     )
+                .WithLogger(new CustomActionLogger("Container", outputHelper.WriteLine))
                 .Build();
+    }
+
+    private static string CreateTempDockerfileForTestcontainer(string source)
+    {
+        var target = $"{source}.tmp";
+
+        var content = File.ReadAllText(source)
+            .Replace(" --platform=$BUILDPLATFORM", "") // not supported by Testcontainers
+            .Replace("-jammy-chiseled-extra", ""); // not supported by Testcontainers because of user permissions
+
+        File.WriteAllText(target, content);
+        return target;
+    }
+
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            await _image.CreateAsync();
+            await _container.StartAsync();
+        }
+        catch (DockerApiException)
+        {
+            var log = await _container.GetLogsAsync();
+            _outputHelper.WriteLine("\nContainer stdOut:\n" + log.Stdout.Replace("\n", "    " + Environment.NewLine));
+            _outputHelper.WriteLine("\nContainer stdErr:\n" + log.Stderr.Replace("\n", "    " + Environment.NewLine));
+            throw;
+        }
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (File.Exists(_tempDockerPath))
+            File.Delete(_tempDockerPath);
+        await _container.StopAsync();
     }
 
     protected override GrpcChannel GetChannel()
@@ -46,14 +101,5 @@ public class ContainerTest : AbstractFlowTest, IAsyncLifetime, IClassFixture<Con
         var address = $"http://localhost:{_container.GetMappedPublicPort(GrpcPort)}";
         return GrpcChannel.ForAddress(address);
     }
-
-    public async Task InitializeAsync()
-    {
-        await _container.StartAsync();
-    }
-
-    public async Task DisposeAsync()
-    {
-        await _container.StopAsync();
-    }
 }
+
---
+++ test/ProjectOrigin.Electricity.IntegrationTests/CustomActionLogger.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectOrigin.Electricity.IntegrationTests;
+
+public class CustomActionLogger : ILogger
+{
+    private readonly Action<string> _outputAction;
+    private readonly string _categoryName;
+
+    public CustomActionLogger(string categoryName, Action<string> outputAction)
+    {
+        _categoryName = categoryName ?? throw new ArgumentNullException(nameof(categoryName));
+        _outputAction = outputAction ?? throw new ArgumentNullException(nameof(outputAction));
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        if (formatter == null)
+        {
+            throw new ArgumentNullException(nameof(formatter));
+        }
+
+        string message = formatter(state, exception);
+        if (!string.IsNullOrEmpty(message))
+        {
+            _outputAction($"{_categoryName}-{logLevel}: {message}");
+        }
+    }
+}
