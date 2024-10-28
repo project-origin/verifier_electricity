57ac8ac7edbd26a7ec51086f1feabd526bec3d40
Philip Hjorth Christiansen <philipchristiansen@gmail.com>
2024-10-28 08:39:01 +0100

    Tests added and IssueKey is made static to avoid errors since TestServerFixture is reused between tests

--- test/ProjectOrigin.Electricity.IntegrationTests/AbstractFlowTest.cs
+++ test/ProjectOrigin.Electricity.IntegrationTests/AbstractFlowTest.cs
@@ -11,6 +11,8 @@ using System.Security.Cryptography;
 using ProjectOrigin.Verifier.V1;
 using System.Collections.Generic;
 using Google.Protobuf.WellKnownTypes;
+using ProjectOrigin.Electricity.V1;
+using ProjectOrigin.Common.V1;
 
 namespace ProjectOrigin.Electricity.IntegrationTests;
 
@@ -25,29 +27,67 @@ public abstract class AbstractFlowTest
 
     public AbstractFlowTest()
     {
-        IssuerKey = Algorithms.Ed25519.GenerateNewPrivateKey();
+        // The same key is used for all tests. A dynamic key caused issued when used together when used
+        // with IClassFixture<TestServerFixture<Startup>> which reuses configuration between tests.
+        IssuerKey = Algorithms.Ed25519.ImportPrivateKeyText("-----BEGIN PRIVATE KEY-----MC4CAQAwBQYDK2VwBCIEIGMlF8WEyXOy8ZSRkfq+TlNSnFdtjs9ONkBDf2fsHMA6-----END PRIVATE KEY-----");
     }
 
     [Fact]
     public async Task IssueConsumptionCertificate_Success()
+    {
+        // Arrange
+        var @event = CreateIssuedEvent();
+
+        // Act
+        var result = await SignEventAndVerify(@event.CertificateId, @event, IssuerKey);
+
+        // Assert
+        result.ErrorMessage.Should().BeEmpty();
+        result.Valid.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task WithdrawCertificate_Success()
+    {
+        // Arrange
+        var @event = CreateIssuedEvent();
+        var issueCertificateTransaction = SignEvent(@event.CertificateId, @event, IssuerKey);
+
+        // Act
+        var result = await SignEventAndVerify(@event.CertificateId, new WithdrawnEvent(), IssuerKey, new[] { issueCertificateTransaction });
+
+        // Assert
+        result.ErrorMessage.Should().BeEmpty();
+        result.Valid.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task UnclaimCertificate_FailedWithMissingCertificateSlice()
+    {
+        // Arrange
+        var issueCertificateEvent = CreateIssuedEvent();
+        var issueCertificateTransaction = SignEvent(issueCertificateEvent.CertificateId, issueCertificateEvent, IssuerKey);
+
+        // Act
+        var result = await SignEventAndVerify(issueCertificateEvent.CertificateId, CreateUnclaimEvent(), IssuerKey, new[] { issueCertificateTransaction });
+
+        // Assert
+        result.Valid.Should().BeFalse();
+        result.ErrorMessage.Should().Be("Certificate claim does not exist");
+    }
+
+    protected static V1.IssuedEvent CreateIssuedEvent()
     {
         var owner = Algorithms.Secp256k1.GenerateNewPrivateKey();
 
         var commitmentInfo = new SecretCommitmentInfo(250);
         var certId = Guid.NewGuid().ToString();
 
-        var @event = new Electricity.V1.IssuedEvent
+        return new V1.IssuedEvent
         {
-            CertificateId = new Common.V1.FederatedStreamId
-            {
-                Registry = Registry,
-                StreamId = new Common.V1.Uuid
-                {
-                    Value = certId
-                },
-            },
-            Type = Electricity.V1.GranularCertificateType.Consumption,
-            Period = new Electricity.V1.DateInterval
+            CertificateId = CreateId(certId),
+            Type = GranularCertificateType.Consumption,
+            Period = new V1.DateInterval
             {
                 Start = Timestamp.FromDateTimeOffset(new DateTimeOffset(2023, 1, 1, 12, 0, 0, 0, TimeSpan.Zero)),
                 End = Timestamp.FromDateTimeOffset(new DateTimeOffset(2023, 1, 1, 13, 0, 0, 0, TimeSpan.Zero))
@@ -64,14 +104,21 @@ public abstract class AbstractFlowTest
                 Content = ByteString.CopyFrom(owner.PublicKey.Export())
             }
         };
-
-        VerifyTransactionResponse result = await SignEventAndVerify(@event.CertificateId, @event, IssuerKey);
-
-        result.ErrorMessage.Should().BeEmpty();
-        result.Valid.Should().BeTrue();
     }
 
-    private async Task<VerifyTransactionResponse> SignEventAndVerify(Common.V1.FederatedStreamId streamId, IMessage @event, IPrivateKey key, IEnumerable<Registry.V1.Transaction>? stream = null)
+    protected static FederatedStreamId CreateId(string certId)
+    {
+        return new FederatedStreamId()
+        {
+            Registry = Registry,
+            StreamId = new Uuid()
+            {
+                Value = certId
+            }
+        };
+    }
+
+    protected async Task<VerifyTransactionResponse> SignEventAndVerify(Common.V1.FederatedStreamId streamId, IMessage @event, IPrivateKey key, IEnumerable<Registry.V1.Transaction>? stream = null)
     {
         var client = new Verifier.V1.VerifierService.VerifierServiceClient(GetChannel());
         var request = new Verifier.V1.VerifyTransactionRequest
@@ -86,7 +133,15 @@ public abstract class AbstractFlowTest
         return result;
     }
 
-    private static Registry.V1.Transaction SignEvent(Common.V1.FederatedStreamId streamId, IMessage @event, IPrivateKey signerKey)
+    protected static UnclaimedEvent CreateUnclaimEvent()
+    {
+        return new UnclaimedEvent
+        {
+            AllocationId = new Uuid() { Value = Guid.NewGuid().ToString() },
+        };
+    }
+
+    protected static Registry.V1.Transaction SignEvent(Common.V1.FederatedStreamId streamId, IMessage @event, IPrivateKey signerKey)
     {
         var header = new Registry.V1.TransactionHeader()
         {
