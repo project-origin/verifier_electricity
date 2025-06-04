3bc35a84b38275ef0d2db92bf3d9c9f0b28a7de0
Martin Schmidt Daugaard <msd@mjolner.dk>
2025-06-04 12:23:17 +0000

    Refactor Match method in IsTrial class for improved readability

--- src/ProjectOrigin.Electricity/Rules/IsTrial.cs
+++ src/ProjectOrigin.Electricity/Rules/IsTrial.cs
@@ -2,17 +2,8 @@ namespace ProjectOrigin.Electricity.Rules;
 
 public static class IsTrial
 {
-    public static bool Match(string? left, string? right)
-    {
-        if (left == right)
-            return true;
-
-        if (left is null && right == "false"
-            || left == "false" && right is null)
-        {
-            return true;
-        }
-
-        return false;
-    }
+    public static bool Match(string? left, string? right) =>
+        (left == right)
+        || (left is null && right == "false")
+        || (left == "false" && right is null);
 }
